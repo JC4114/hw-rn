e158a5c450847166cd298566f024f0b0c4e65c3f
JC4114 <4114114jc@gmail.com>
2023-03-31 14:30:44 +0300

    hw2

--- App.js
+++ App.js
@@ -1,15 +1,26 @@
-import { ImageBackground, StyleSheet, TextInput, View } from 'react-native'
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import { useFonts } from "expo-font";
+
+// import LoginScreen from "./Screens/LoginScreen/LoginScreen";
+import RegistrationScreen from "./Screens/RegistrationScreen/RegistrationScreen";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <ImageBackground 
-      source={require("./assets/ae59d8cc3aacd26854e382af3478836a.jpg")}
-      style={styles.image}
-      
-      >
-<TextInput style={styles.input}/>
-      </ImageBackground>
+      <RegistrationScreen />
+      {/* <LoginScreen /> */}
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -17,18 +28,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-
-  image:{
-    flex:1,
-  resizeMode:'cover',
-justifyContent:'center',
-alignItems:'center'},
-input:{
-  borderWidth:1,
-  borderColor:'#fff'
-}
 });
